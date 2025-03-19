4fb8196fbd46bab343fa24561741dc0ffd174f40
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2025-03-19 21:08:07 +0530

    Mainstream news data test

---
+++ Downforce/APIManager.swift
@@ -0,0 +1,62 @@
+//
+//  APIManager.swift
+//  Downforce
+//
+//  Created by Ayush Bhople on 19/03/25.
+//
+
+import Foundation
+
+protocol MainNewsFetcherDelegate {
+    func didFetchMainNews(_ apiManager: APIManager, mainNewsModel: MainNewsModel)
+    func didFailWithError(error: Error)
+}
+
+struct APIManager {
+    
+    var delegate: MainNewsFetcherDelegate?
+    
+    let mainNewsUrl = "https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=8acdf1145d14476091edab5f5f8a5cd7"
+    
+    func fetchMainStreamCNNNews() {
+        fetchNews(with: mainNewsUrl)
+    }
+    
+    func fetchNews(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if (error != nil){
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let parsedData = parseJSONForMainNews(safeData) {
+                        self.delegate?.didFetchMainNews(self, mainNewsModel: parsedData)
+                    }
+                }
+            }
+            task.resume()
+        }
+        
+    }
+    
+    func parseJSONForMainNews(_ newsData: Data) -> MainNewsModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(MainNewsModel.self, from: newsData)
+            let totalResults = decodedData.totalResults
+            
+            let news = MainNewsModel(totalResults: totalResults, articles: nil)
+            return news
+        }
+        catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+}
+
+
+
--- Downforce/ViewController.swift
+++ Downforce/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalCount: UILabel!
+    
+    var apiManager = APIManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        apiManager.delegate = self
+        
+        apiManager.fetchMainStreamCNNNews()
         
     }
 
 
+}
+
+
+extension ViewController: MainNewsFetcherDelegate {
+    func didFetchMainNews(_ apiManager: APIManager, mainNewsModel: MainNewsModel) {
+        DispatchQueue.main.async {
+            self.totalCount.text = "\(mainNewsModel.totalResults ?? 0)"
+        }
+    }
+    
+    func didFailWithError(error: any Error) {
+        
+    }
+    
+    
 }
 
