abf40c45a09dd645b143988b3b6e51642cb005fd
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2025-03-19 21:08:07 +0530

    Mainstream news data test

---
+++ Downforce/MainNewsModel.swift
@@ -0,0 +1,26 @@
+//
+//  MainNewsModel.swift
+//  Downforce
+//
+//  Created by Ayush Bhople on 19/03/25.
+//
+
+import Foundation
+
+struct MainNewsModel: Codable {
+    let totalResults: Int?
+    let articles: [MainNews]?
+}
+
+struct MainNews: Codable {
+    let source: Source
+    let title: String
+    let description: String
+    let url: String
+    let urlToImage: String
+}
+
+struct Source: Codable {
+    let id: String
+    let name: String
+}
