529149ca7e2089e9751a149cfe69c545b55de162
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2025-05-23 11:53:46 +0530

    script change 2 (refresh)

--- scripts/inject_google_service_info.sh
+++ scripts/inject_google_service_info.sh
@@ -4,12 +4,24 @@ set -e
 
 echo "Injecting GoogleService-Info.plist...."
 
+echo "CI_WORKSPACE = $CI_WORKSPACE"
+
+# Check if variable is set
+if [ -z "$GOOGLE_SERVICE_INFO_PLIST" ]; then
+  echo "GOOGLE_SERVICE_INFO_PLIST is not set!"
+  exit 1
+fi
+
+# Ensure directory exists
 mkdir -p "$CI_WORKSPACE/Downforce"
 
+echo "Before injection:"
 ls -l "$CI_WORKSPACE/Downforce"
 
+# Decode and write plist
 echo "$GOOGLE_SERVICE_INFO_PLIST" | base64 --decode > "$CI_WORKSPACE/Downforce/GoogleService-Info.plist"
 
+echo "After injection:"
 ls -l "$CI_WORKSPACE/Downforce"
 
 echo "GoogleService-Info.plist injected"
