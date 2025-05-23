f480f93dcd35488b0d08c983415616e6659b94a6
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2025-05-23 11:45:17 +0530

    changes in the script (refresh)

--- scripts/inject_google_service_info.sh
+++ scripts/inject_google_service_info.sh
@@ -4,6 +4,12 @@ set -e
 
 echo "Injecting GoogleService-Info.plist...."
 
+mkdir -p "$CI_WORKSPACE/Downforce"
+
+ls -l "$CI_WORKSPACE/Downforce"
+
 echo "$GOOGLE_SERVICE_INFO_PLIST" | base64 --decode > "$CI_WORKSPACE/Downforce/GoogleService-Info.plist"
+
+ls -l "$CI_WORKSPACE/Downforce"
 
 echo "GoogleService-Info.plist injected"
